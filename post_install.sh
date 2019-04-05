d35eecf60a22640247099ab2744b9c6509d7b5bb
Jurgen Segaert <jsegaert@yahoo.com>
2019-04-04 21:05:48 -0700

    refine post_install script

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-mkdir -p /usr/local/jdownloader
-mkdir -p /usr/local/etc/rc.d
+# Defaults
+USER_NAME=jdownloader
+USER_UID=65310
+DIR_HOME=/home/$USER_NAME
+DIR_INSTALL=/usr/local/jdownloader
 
-pw useradd -n jdownloader -c JDownloader\ user -u 65310 -d /nonexistent -s /usr/sbin/nologin
 
-cd /usr/local/jdownloader
+# Prepare
+mkdir -p $DIR_HOME
+mkdir -p $DIR_INSTALL
+pw useradd -n $USER_NAME -c JDownloader\ user -u $USER_UID -d DIR_HOME -s /usr/sbin/nologin
+
+
+# Download 
+cd $DIR_INSTALL
 fetch http://installer.jdownloader.org/JDownloader.jar
+echo "JDownloader downloaded" > /root/PLUGIN_INFO
 
-#TODO
-#/usr/local/bin/java -Djava.awt.headless=true -jar /usr/local/jdownloader/JDownloader.jar -norestart
+
+# Install & update
+/usr/local/bin/java -Djava.awt.headless=true -jar $DIR_INSTALL/JDownloader.jar -norestart
+chown -R $USER_NAME:$USER_NAME .
+echo "JDownloader installed" > /root/PLUGIN_INFO
+
+
+# Set up the service
+sysrc -f /etc/rc.conf jdownloader_enable="YES"
+sysrc -f /etc/rc.conf jdownloader_user="$USER_NAME"
 
