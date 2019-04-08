3c4125a7e957d182a88541c55819ce790fa6b6e7
Jurgen Segaert <jsegaert@yahoo.com>
2019-04-07 22:45:08 -0700

    Add rc.d and setter file

--- post_install.sh
+++ post_install.sh
@@ -3,26 +3,28 @@
 # Defaults
 USER_NAME=jdownloader
 USER_UID=65310
-DIR_HOME=/home/$USER_NAME
 DIR_INSTALL=/usr/local/jdownloader
 
 
 # Prepare
-mkdir -p $DIR_HOME
 mkdir -p $DIR_INSTALL
-pw useradd -n $USER_NAME -c JDownloader\ user -u $USER_UID -d DIR_HOME -s /usr/sbin/nologin
+pw useradd -n $USER_NAME -c JDownloader\ user -u $USER_UID -m -s /bin/tcsh
 
 
 # Download 
 cd $DIR_INSTALL
 fetch http://installer.jdownloader.org/JDownloader.jar
-echo "JDownloader downloaded" > /root/PLUGIN_INFO
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Failed to download http://installer.jdownloader.org/JDownloader.jar" > /root/PLUGIN_INFO
+  exit 1
+fi
 
 
 # Install & update
 /usr/local/bin/java -Djava.awt.headless=true -jar $DIR_INSTALL/JDownloader.jar -norestart
+cp /usr/local/etc/JDownloader-InitialSettings.json $DIR_INSTALL/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
 chown -R $USER_NAME:$USER_NAME .
-echo "JDownloader installed" > /root/PLUGIN_INFO
+echo "JDownloader was succesfully installed" > /root/PLUGIN_INFO
 
 
 # Set up the service
