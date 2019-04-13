5a64abebc69e89566ce16fbbc83f0d505cf66a66
Jurgen Segaert <jsegaert@yahoo.com>
2019-04-12 21:15:00 -0700

    Configure ffmpeg

--- post_install.sh
+++ post_install.sh
@@ -5,11 +5,9 @@ USER_NAME=jdownloader
 USER_UID=65310
 DIR_INSTALL=/usr/local/jdownloader
 
-
 # Prepare
 mkdir -p $DIR_INSTALL
 pw useradd -n $USER_NAME -c JDownloader\ user -u $USER_UID -m -s /bin/tcsh
-
 
 # Download 
 cd $DIR_INSTALL
@@ -19,15 +17,26 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
-
 # Install & update
 /usr/local/bin/java -Djava.awt.headless=true -jar $DIR_INSTALL/JDownloader.jar -norestart
 cp /usr/local/etc/JDownloader-InitialSettings.json $DIR_INSTALL/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json
+cp /usr/local/etc/JDownloader-FFmpegSetup.json $DIR_INSTALL/cfg/org.jdownloader.controlling.ffmpeg.FFmpegSetup.json
 chown -R $USER_NAME:$USER_NAME .
-echo "JDownloader was succesfully installed" > /root/PLUGIN_INFO
-
 
 # Set up the service
 sysrc -f /etc/rc.conf jdownloader_enable="YES"
 sysrc -f /etc/rc.conf jdownloader_user="$USER_NAME"
+
+# Provide instructions
+cat << EOF > /root/PLUGIN_INFO
+
+***** The JDownloader plugin is installed *****
+
+To complete the setup:
+ - register for a free account at https://my.jdownloader.org
+ - run the following command to link to your account to the plugin:
+     jdownloaderinit <email> <password>
+
+EOF
+cat /root/PLUGIN_INFO
 
